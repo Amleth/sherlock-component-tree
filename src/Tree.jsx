9ee2893a5506bf9b10d6dbb5fc8851bb0ec0ba88
Thomas Bottini <thomas.bottini@cnrs.fr>
2021-03-24 09:03:49 +0100

    Premier commit

---
+++ src/Tree.jsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { style } from "./style.css";
+
+const Tree = ({}) => {
+  return <div css={style}>شجرة</div>;
+};
+
+export default Tree;
---
+++ src/style.css.js
@@ -0,0 +1,7 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+export const style = css`
+    color: purple;
+    font-size: 4em;
+`
